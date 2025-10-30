94b3a03d411db7a2edfb1020c37b7094acf2369f
Matthieu CRON <matthieu.cron@orange.fr>
2025-10-30 15:39:58 +0100

    Remplacement des \n par des <br> et mis dans le "bon" ordre les champ pour crée le track (car sinon mauvau type).

--- Repo.php
+++ Repo.php
@@ -8,16 +8,17 @@ $repo = \iutnc\deefy\repository\DeefyRepository::getInstance();
 
 $playlists = $repo->recupererToutesPlaylists();
 foreach ($playlists as $pl) {
-    print "playlist  : " . $pl->nom . ":". $pl->id . "   \n";
+    print "playlist  : " . $pl->nom . ":". $pl->id . "   <br>";
 }
 
 
 $pl = new \iutnc\deefy\audio\lists\PlayLists('test');
 $pl = $repo->sauvegarderPlaylistVide($pl);
-print "playlist  : " . $pl->nom . ":". $pl->id . "\n";
+print "playlist  : " . $pl->nom . ":". $pl->id . "<br>";
 
-$track = new \iutnc\deefy\audio\tracks\PodcastTrack('test', 'test.mp3', 'auteur', '2021-01-01', 10, 'genre');
+
+$track = new \iutnc\deefy\audio\tracks\PodcastTrack('2021-01-01', 'auteur', 'test', 'genre', 10, 'test.mp3');
 $track = $repo->sauvegarderTrack($track);
-print "track 2 : " . $track->titre . ":". get_class($track). "\n";
+print "track 2 : " . $track->titre . ":". get_class($track). "<br>";
 $repo->ajouterTrackAPlaylist($pl->id, $track->id);
 
